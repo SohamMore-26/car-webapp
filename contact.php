4d962d6eeedbea82ca0007b06963c3befb79817a
uvjoshi <uvjoshi2005@gmail.com>
2023-08-05 23:51:59 +0530

    ...

--- contact.php
+++ contact.php
@@ -70,16 +70,22 @@
   <!-- END nav -->
 
   <section class="hero-wrap hero-wrap-3 js-fullheight"
-    style="background-image: url('images/damian-zaleski-RYyr-k3Ysqg-unsplash.jpg');"
-    data-stellar-background-ratio="0.5">
-    <div class="container">
-      <div class="row no-gutters slider-text js-fullheight align-items-end justify-content-start">
-        <div class="col-md-9 ftco-animate pb-5">
-          <h1 class="mb-3 bread">Contact</h1>
+        style="background-image: url('images/damian-zaleski-RYyr-k3Ysqg-unsplash.jpg');"
+        data-stellar-background-ratio="0.5">
+        <div class="overlay"></div>
+        <div class="container">
+            <div class="row no-gutters slider-text js-fullheight align-items-end justify-content-start">
+                <div class="col-md-9 ftco-animate pb-5">
+                    <div class="breadup">
+                        <p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home <i
+                                        class="ion-ios-arrow-forward"></i></a></span> <span>Contact us <i
+                                    class="ion-ios-arrow-forward"></i></span></p>
+                        <h1 class="mb-3 bread">Contact Us</h1>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  </section>
+    </section>
 
   <section class="ftco-section contact-section">
     <div class="container">
